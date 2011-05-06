6f0164e1be637b7b135cc1ba4f7e701dec22d30b
gearon@gmail.com <gearon@gmail.com@c0e7bae4-bc0b-de1c-f79b-81ecdbaab861>
2011-05-06 19:06:43 +0000

    Tagging 0.7 release

---
+++ tags/mrg-0.7/src/main/java/org/mulgara/util/Fn2E.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2010 Paul Gearon.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.mulgara.util;
+
+/**
+ * Functor template for a function that takes arguments of two different types
+ * and returns a value of a third type, possibly throwing an exception.
+ * @param <T1> The type of the first parameter of the function.
+ * @param <T2> The type of the second parameter of the function.
+ * @param <R> The return type of the function.
+ * @param <E> The type of exception that may be thrown from the function.
+ *
+ * @author Paul Gearon
+ */
+public interface Fn2E<T1,T2,R,E extends Exception> {
+
+  /**
+   * Declares a function template that takes two arguments and returns a value of
+   * another type.
+   * @param arg1 The first argument.
+   * @param arg2 The second argument.
+   * @return A value based on arg1 and arg2.
+   * @throws E An exception that may be thrown from this method.
+   */
+  R call(T1 arg1, T2 arg2) throws E;
+}
